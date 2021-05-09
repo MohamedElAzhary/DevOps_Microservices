c42da9381d9c1199c1184bb8d490bbde176c9cc3
Ubuntu <ubuntu@ip-172-31-4-39.us-west-2.compute.internal>
2021-05-09 23:53:54 +0000

    Adding wikiapp image

--- Lesson-1-Lambda-functions/wikipedia-query/wiki/wikipedia_lambda_solution.py
+++ Lesson-1-Lambda-functions/wikipedia-query/wiki/wikipedia_lambda_solution.py
@@ -11,11 +11,11 @@ def lambda_handler(event, context):
         :return: a response that contains the first sentence of a wikipedia page,
             the response is JSON formatted.'''
     
-    ## TO DO: Check that the request has some input body and save it
+    # check that the request has some input body
     if 'body' in event:
         body = json.loads(event["body"])
     
-    ## TO DO: Get the wikipedia "entity" from the body of the request
+    # get the wikipedia "entity" from the body of the request
     entity = body["entity"]
     res = wikipedia.summary(entity, sentences=1) # first sentence, result
 
@@ -23,7 +23,7 @@ def lambda_handler(event, context):
     print(f"context: {context}, event: {event}")
     print(f"Response from wikipedia API: {res}")
     
-    ## TO DO: Format the response as JSON and return the result
+    # format the response as JSON and return the result
     response = {
         "statusCode": "200", 
         "headers": { "Content-type": "application/json" },
