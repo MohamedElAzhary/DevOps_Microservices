5340685f55eda967c678aab782984dd5b26cbb56
EC2 Default User <ec2-user@ip-172-31-15-173.us-west-2.compute.internal>
2021-05-22 12:26:42 +0000

    Fixing run_kubernetes and submitting output

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,17 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=moazario/mlproject:ml
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl create deployment mldeployment --image=ml
+kubectl run mlpod --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
 kubectl get pods
+sleep 2m
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward mldeployment 8000:80
+kubectl port-forward mlpod 8000:80
 
