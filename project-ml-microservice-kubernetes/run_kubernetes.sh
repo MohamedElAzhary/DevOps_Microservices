45a67b5f66234ef7a27cc3f623a6461465cc7072
EC2 Default User <ec2-user@ip-172-31-15-173.us-west-2.compute.internal>
2021-05-22 11:39:14 +0000

    Modifying run_kubernetes.sh

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -8,11 +8,13 @@
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment mldeployment --image=ml
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward mldeployment 8000:80
 
